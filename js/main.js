0151f05c6e92e4bfb4a4a79d82bcc71d984fcc8c
Tyrone Ruffin <tyruff@fullsail.edu>
2013-03-14 18:58:22 -0500

    done

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
-
-
+// Tyrone Ruffin
+// Project 2
+// VFW 1303
+// 03-14-2013
 
 
 
@@ -27,6 +29,45 @@ window.addEventListener("DOMContentLoaded", function(){
           selectLi.appendChild(makeSelect);
        
     }
+    // Reads as null, what im I doing wrong?
+    function getRadio() {
+        var radios = document.forms[0].location
+        for(var i=0; i<radios.length; i++){
+            if(radios[i].checked){
+                locationValue = radios[i].value;
+            }
+        }
+    }
+    // Also reads as null.
+    function checkBox(){
+        if($("action").checked){
+            genreValue = $("action").value;
+        }else{
+            genreValue = "Not Action"
+        
+        }
+    }
+    
+    function toggleControl(n){
+        switch(n){
+            case "on":
+                $("contactForm").style.display = "none";
+                $("clear").style.display = "inline";
+                $("show").style.display = "none"
+                $("addNew").style.display = "inline";
+                break;
+            case "off":
+                $("contactForm").style.display = "block";
+                $("clear").style.display = "inline";
+                $("show").display = "inline"
+                $("addNew").style.display = "none";
+                $("items").style.display = "none";
+                break;
+            default:
+                
+        }
+        
+    };
     
     function storeData(){
         var id              = Math.floor(Math.random()*10000001);
@@ -35,27 +76,72 @@ window.addEventListener("DOMContentLoaded", function(){
             item.username   = ["Username", $("username").value];
             item.password   = ["Password", $("password").value];
             item.title      = ["Movie Title", $("title").value];
-          /*  item.genre      = ["Genre", genreValue ];*/
+            item.genre      = ["Genre", genreValue ];
             item.comment    = ["Comments", $("comments").value];
-         /*   item.location   = ["Location", locationValue];*/
+            item.location   = ["Location", locationValue];
             item.rating     = ["Rating", $("rating").value];
+            item.length     = ["Movie Length", $("movielength").value];
         localStorage.setItem(id, JSON.stringify(item));
         alert("Title Saved");
     }
+    
+    function getData(){
+        toggleControl("on");
+        if(localStorage.length === 0){
+            alert("No Titles Saved");
+        }
+        var makeDiv = document.createElement("div");
+        makeDiv.setAttribute("id", "items");
+        var makeList = document.createElement("ul");
+        makeDiv.appendChild(makeList);
+        document.body.appendChild(makeDiv);
+        $("items").style.display = "block";
+        for(var i = 0, len=localStorage.length; i < len; i++){
+            var makeli = document.createElement("li");
+            makeList.appendChild(makeli);
+            var key = localStorage.key(i);
+            var value = localStorage.getItem(key);
+            var object = JSON.parse (value);
+            var makeSubList = document.createElement("ul");
+            makeli.appendChild(makeSubList);
+            for(var n in object){
+                var makeSubli = document.createElement("li");
+                makeSubList.appendChild(makeSubli);
+                var optSubText = object[n][0] + " " + object[n][1];
+                makeSubli.innerHTML = optSubText;
+            }
+        }
+    }
+    
+    function clearLocal(){
+        if(localStorage.length === 0){
+            alert("No Movie Titles Found.")
+        }else{
+            localStorage.clear();
+            alert("All Titles deleted")
+            window.location.reload();
+            return false;
+        }
+    }
+   
+    
+    
 
-    var userInfo = ["--Which are you?--", "Administrator", "Guest"];
+    var userInfo = ["--Which are you?--", "Administrator", "Guest"],
+        locationValue ,genreValue;
+        
     makeCats();
     
 
 
-/*
-    var show = $('show');
-    show.addEventListener("click", getData);
+
+    
     var clearLink = $('clear');
-    clearLink.addEventListener("click", clearLocal);*/
+    clearLink.addEventListener("click", clearLocal);
     var save = $('save');
     save.addEventListener("click", storeData);
-
+    var displayLink = $('show');
+    displayLink.addEventListener("click", getData);
 
 
 
